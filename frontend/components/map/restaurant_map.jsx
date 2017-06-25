d4be241cfce699b70575401395b1d0cf6047ac0e
Travis Greenbaum <TravisMG11@gmail.com>
2017-06-25 14:42:39 -0400

    Begin map feature setup

---
+++ frontend/components/map/restaurant_map.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { withRouter } from 'react-router-dom';
+
+class RestaurantMAp extends React.Component {
+
+
+  render() {
+    return (
+      <div className="map">
+        
+      </div>
+    )
+  }
+}
