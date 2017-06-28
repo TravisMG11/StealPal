59d3936d46be10077e0b99c1fe016860fe89aceb
Zack Wilder <zwilder@gmail.com>
2017-06-28 14:12:17 -0400

    Update filters

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -12,13 +12,7 @@ const SearchBar = (props) => {
     "Vegetarian"
   ];
 
-  const createCheckbox = (label) => {
-    return <Checkbox label={label} key={label} />;
-  };
 
-  const createCheckboxes = () => (
-    items.map(this.createCheckbox)
-  );
 
   return (
     <div className="search-bar-div">
@@ -26,9 +20,69 @@ const SearchBar = (props) => {
         <input className="city-input" value={"NEW YORK CITY"} />
         <input className="meal-input" placeholder="Search by restaurant or meal" value={props.searchTerm} onChange={handleChange} />
         <input className="loco-input"value={"Search by location"} />
+          <div className="check-row">
+            <div className="check-field" >
+              <input className="check-box" type="checkbox" checked={false}/>
+              <label className="checkbox-label">American</label>
+            </div>
+            <div className="check-field" >
+              <input className="check-box" type="checkbox" checked={false}/>
+              <label className="checkbox-label">Mexican</label>
+            </div>
+            <div className="check-field" >
+              <input className="check-box" type="checkbox" checked={false}/>
+              <label className="checkbox-label">Rugged</label>
+            </div>
+            <div className="check-field" >
+              <input className="check-box" type="checkbox" checked={false}/>
+              <label className="checkbox-label">Cartoon</label>
+            </div>
+            <div className="check-field" >
+              <input className="check-box" type="checkbox" checked={false}/>
+              <label className="checkbox-label">Italian</label>
+            </div>
+          </div>
+          <div className="check-row">
+            <div className="check-field" >
+              <input className="check-box" type="checkbox" checked={false}/>
+              <label className="checkbox-label">Light Lunch</label>
+            </div>
+            <div className="check-field" >
+              <input className="check-box" type="checkbox" checked={false}/>
+              <label className="checkbox-label">Mid Size</label>
+            </div>
+            <div className="check-field" >
+              <input className="check-box" type="checkbox" checked={false}/>
+              <label className="checkbox-label">Lots O Food</label>
+            </div>
+          </div>
       </form>
     </div>
   );
 };
+
+// const createCheckbox = (label) => (
+//   <input type="checkbox" label={label} key={label} />
+// );
+//
+// const createCheckboxes = () => (
+//   cuisines.forEach(function(label) {
+//     return createCheckbox(label);
+//   }
+//   )
+// );
+//
+// return (
+//   <div className="search-bar-div">
+//     <form className="search-bar">
+//       <input className="city-input" value={"NEW YORK CITY"} />
+//       <input className="meal-input" placeholder="Search" value={props.searchTerm} onChange={handleChange} />
+//       <input className="loco-input"value={"Search by location"} />
+//         <div>
+//           {createCheckboxes()}
+//       </div>
+//     </form>
+//   </div>
+// );
 
 export default SearchBar;
